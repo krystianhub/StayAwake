1ad477cdf4b7cdb96eb8df3b0966e54041fbe6a6
Krystian <krystian.inbox@gmail.com>
2022-02-28 15:22:51 +0000

    Fix

--- build.rs
+++ build.rs
@@ -2,7 +2,8 @@ use std::env;
 
 fn main() {
     let target_os = env::var("CARGO_CFG_TARGET_OS");
-    if let Ok("macos") = target_os.as_ref().map(|x| x.as_str()) {
+
+    if let Ok("macos") = target_os.as_deref() {
         println!("cargo:rustc-link-lib=framework=IOKit")
     }
 }
